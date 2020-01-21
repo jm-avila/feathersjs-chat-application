d030164f1dea9defbf4bd767e2d7d224fd603a47
jm-avila <jm.avila1309@icloud.com>
2020-01-21 19:00:22 +0100

    Login page working

---
+++ public/js/login.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+  var serverurl = "http://localhost:3030";
+
+  // feathers boilerplate code to connect to users service
+  var socket = io(serverurl);
+  // initialize our feathers client application through socket.io
+  var client = feathers();
+  client.configure(feathers.socketio(socket));
+  // use localstorage to store jwt
+  client.configure(
+    feathers.authentication({
+      storage: window.localStorage
+    })
+  );
+
+  // obtain users service
+
+  // Get User Credentials
+  function getCredentials() {
+    var user = {
+      email: $("#email").val(),
+      password: $("#password").val()
+    };
+
+    return user;
+  }
+
+  // Handle form submittal
+  $("#login-user-form").submit(function(e) {
+    e.preventDefault();
+
+    var userCredentials = getCredentials();
+
+    // Authenticate with feathers client:
+    // success: redirect to app
+    // error: error message
+
+    client
+      .authenticate({
+        strategy: "local",
+        email: userCredentials.email,
+        password: userCredentials.password
+      })
+      .then(token => {
+        window.location.href = serverurl;
+      })
+      .catch(err =>
+        $("#error-message")
+          .text(`There was an error ${err}`)
+          .show()
+      );
+  });
+});
