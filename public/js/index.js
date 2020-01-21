673cb5a009ba248492ab2d59c8f822d187583f8f
jm-avila <jm.avila1309@icloud.com>
2020-01-21 21:04:52 +0100

    Logout button working

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  var serverurl = "http://localhost:3030";
+
+  // feathers boilerplate code to connect to users service
+  var socket = io(serverurl);
+  // initialize our feathers client application through socket.io
+  var client = feathers();
+  client.configure(feathers.socketio(socket));
+  // use localstorage to store jwt
+  client.configure(
+    feathers.authentication({
+      storage: window.localStorage
+    })
+  );
+
+  // obtain users service
+  var userService = client.service("/users");
+
+  // Handle form submittal
+  $("#logout-icon").on("click", function(e) {
+    e.preventDefault();
+
+    // Logout is clicked
+    client.logout();
+    window.location.href = `${serverurl}/login.html`;
+  });
+});
