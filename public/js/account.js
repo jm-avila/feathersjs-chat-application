98838529f3abc77d5036a55ccd6163f0cef93b1c
jm-avila <jm.avila1309@icloud.com>
2020-01-21 18:40:55 +0100

    Create a new account page working

--- public/js/account.js
+++ public/js/account.js
@@ -6,4 +6,43 @@ $(document).ready(function() {
   // initialize our feathers client application through socket.io
   var client = feathers();
   client.configure(feathers.socketio(socket));
+  // use localstorage to store jwt
+  client.configure(
+    feathers.authentication({
+      storage: window.localStorage
+    })
+  );
+
+  // obtain users service
+  var userService = client.service("/users");
+
+  // Get User Credentials
+  function getCredentials() {
+    var user = {
+      email: $("#email").val(),
+      password: $("#password").val()
+    };
+
+    return user;
+  }
+
+  // Handle form submittal
+  $("#new-user-form").submit(function(e) {
+    e.preventDefault();
+
+    var userCredentials = getCredentials();
+
+    // Create a user using feathers client
+
+    userService
+      .create(userCredentials)
+      .then(res => {
+        window.location.href = `${serverurl}/login.html`;
+      })
+      .catch(err => {
+        $("#error-message")
+          .text(`There was an error ${err}`)
+          .show();
+      });
+  });
 });
