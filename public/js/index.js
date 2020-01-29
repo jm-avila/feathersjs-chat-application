a8a4ce8916e804f453288618eb9153cebddcf5aa
jm-avila <jm.avila1309@icloud.com>
2020-01-29 07:47:23 +0100

    Delete message button only available for the user who created the message

--- public/js/index.js
+++ public/js/index.js
@@ -20,15 +20,15 @@ $(document).ready(function() {
   var getMessages = async () => {
     var messages = await messageService.find({
       query: {
-        $limit: 2,
+        $limit: 5,
         $sort: { createdAt: -1 }
       }
     });
 
     var htmlMessages = messages.data
       .sort((a, b) => (new Date(a.createdAt) > new Date(b.createdAt) ? 1 : -1))
-      .map(message =>
-        new Message(message.text, message.createdAt).getMessageHtmlString()
+      .map(({ text, createdAt, userId }) =>
+        new Message(text, createdAt, userId).getMessageHtmlString()
       );
 
     return htmlMessages;
@@ -36,36 +36,42 @@ $(document).ready(function() {
 
   // Message class - Handle message production
   class Message {
-    constructor(msgText, createdAt) {
+    constructor(msgText, createdAt, msgUserId = null) {
       this.msgText = msgText;
       this.createdAt = createdAt;
+      this.msgUserId = msgUserId;
     }
 
     getMessageHtmlString() {
-      var msgHtmlString = `<div class="media mt-3">
-      <div class="media-left mr-3">
-        <a href="#">
-          <img
-            src="https://img.favpng.com/23/0/3/computer-icons-user-profile-clip-art-portable-network-graphics-png-favpng-YEj6NsJygkt6nFTNgiXg9fg9w.jpg"
-            alt="64x64 user image"
-            class="media-object"
-            style="width: 64px; height: 64px;"
-          />
-        </a>
-      </div>
-      <div class="media-body border-bottom">
+      var deleteIconHtml = "";
+      if (this.msgUserId === client.get("userId")) {
+        deleteIconHtml = `
         <div class="float-right">
           <span class="delete-comment" title="Delete Comment">
             <i class="fa fa-times" aria-hidden="true"></i>
           </span>
         </div>
-        <h4 class="media-heading">${"CHemi"}</h4>
-        <p class="mb-3">${this.createdAt}</p>
-        <p>
-         ${this.msgText}
-        </p>
-      </div>
-    </div>`;
+      `;
+      }
+      var msgHtmlString = `
+      <div class="media mt-3">
+        <div class="media-left mr-3">
+          <a href="#">
+            <img
+              src="https://img.favpng.com/23/0/3/computer-icons-user-profile-clip-art-portable-network-graphics-png-favpng-YEj6NsJygkt6nFTNgiXg9fg9w.jpg"
+              alt="64x64 user image"
+              class="media-object"
+              style="width: 64px; height: 64px;"
+            />
+          </a>
+        </div>
+        <div class="media-body border-bottom">
+          ${deleteIconHtml}
+          <h4 class="media-heading">${"CHemi"}</h4>
+          <p class="mb-3">${this.createdAt}</p>
+          <p>${this.msgText}</p>
+        </div>
+      </div>`;
       return msgHtmlString;
     }
   }
@@ -73,46 +79,54 @@ $(document).ready(function() {
   // Is user authenticated - run page code ELSE redirecto to login page
   client
     .authenticate()
-    .then(response => {
-      $("#logout-icon").on("click", function(e) {
-        e.preventDefault();
-
-        // Logout is clicked
-        client.logout();
-        window.location.href = `${serverurl}/login.html`;
-      });
-
-      getMessages().then(htmlMessages => $("#chat-area").append(htmlMessages));
-
-      // Handle form submittal
-      $("#submit-message-form").submit(function(e) {
-        e.preventDefault();
-        var $msgText = $("#msg-text");
-        var msgText = $msgText.val();
-        $msgText.val("");
-
-        // if message text contains more than whitespace, save the message to the database.
-        if (msgText.trim().length) {
-          messageService
-            .create({
-              text: msgText
-            })
-            .catch(err => {
-              alert(`There was an error: ${err.message}`);
-            });
-        }
-      });
-
-      // Watch for new message events and handle
-      messageService.on("created", message => {
-        var newMessage = new Message(message.text, message.createdAt);
-
-        $("#chat-area").append(newMessage.getMessageHtmlString());
-
-        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-      });
+    .then(response => client.passport.verifyJWT(response.accessToken))
+    .then(payload => {
+      const userId = payload.sub;
+      client.set("userId", userId);
+      main();
     })
     .catch(err => {
       window.location.href = `${serverurl}/login.html`;
     });
+
+  // function runs all page load script after authentication is completed.
+  function main() {
+    $("#logout-icon").on("click", function(e) {
+      e.preventDefault();
+
+      // Logout is clicked
+      client.logout();
+      window.location.href = `${serverurl}/login.html`;
+    });
+
+    getMessages().then(htmlMessages => $("#chat-area").append(htmlMessages));
+
+    // Handle form submittal
+    $("#submit-message-form").submit(function(e) {
+      e.preventDefault();
+      var $msgText = $("#msg-text");
+      var msgText = $msgText.val();
+      $msgText.val("");
+
+      // if message text contains more than whitespace, save the message to the database.
+      if (msgText.trim().length) {
+        messageService
+          .create({
+            text: msgText
+          })
+          .catch(err => {
+            alert(`There was an error: ${err.message}`);
+          });
+      }
+    });
+
+    // Watch for new message events and handle
+    messageService.on("created", ({ text, createdAt, userId }) => {
+      var newMessage = new Message(text, createdAt, userId);
+
+      $("#chat-area").append(newMessage.getMessageHtmlString());
+
+      $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+    });
+  }
 });
